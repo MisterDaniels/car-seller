c8c96fe99cf89ffadb22b5444d43c096eaacbb96
Daniel Silva <dodosilva51@gmail.com>
2021-09-16 21:49:45 -0300

    Added other links in footer and some layout changes

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,16 +1,17 @@
+import { faEnvelope, faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import styled from 'styled-components';
 import tw from 'twin.macro';
 import { Logo } from '../Logo';
 
 const FooterContainer = styled.div`
-    min-height: 30em;
     background-color: #1d2124;
     ${tw`
         flex
         min-w-full
-        pt-4
-        md:pt-12
+        pt-10
+        md:pt-16
         pb-1
         items-center
         justify-center
@@ -20,9 +21,11 @@ const FooterContainer = styled.div`
 const InnerContainer = styled.div`
     ${tw`
         flex
+        flex-wrap
         w-full
         h-full
         max-w-screen-2xl
+        justify-center
     `};
 `;
 
@@ -31,7 +34,11 @@ const AboutContainer = styled.div`
         flex
         flex-col
         mr-2
-        md:mr-10
+        md:mr-16
+        pl-10
+        pr-10
+        md:pl-3
+        md:pr-3
     `};
 `;
 
@@ -48,9 +55,18 @@ const AboutText = styled.p`
 
 const SectionContainer = styled.div`
     ${tw`
+        w-full
+        md:w-auto
         flex
         flex-col
-        mr-3
+        mr-2
+        md:mr-16
+        pl-10
+        pr-10
+        md:pl-3
+        md:pr-3
+        mt-7
+        md:mt-0
     `};
 `;
 
@@ -72,17 +88,48 @@ const ListItem = styled.li`
         ${tw`
             text-sm
             text-white
+            transition-all
+            hover:text-gray-200
         `};
     };
 `;
 
 const HeaderTitle = styled.h3`
     ${tw`
-        text-base
+        text-2xl
         font-bold
         text-white
         mb-3
     `};
+`;
+
+const HorizontalContainer = styled.div`
+    ${tw`
+        flex
+        items-center
+    `}
+`;
+
+const RedIcon = styled.span`
+    ${tw`
+        w-9
+        h-9
+        rounded-full
+        bg-red-500
+        flex
+        items-center
+        justify-center
+        text-white
+        text-base
+        mr-2
+    `};
+`;
+
+const SmallText = styled.h6`
+    ${tw`
+        text-sm
+        text-white
+    `}
 `;
 
 export function Footer() {
@@ -95,40 +142,92 @@ export function Footer() {
                         Yourcar is a car renting and selling company located in many countries across the world which has high quality cars and top rated
                     </AboutText>
                 </AboutContainer>
-                <LinksList>
-                    <SectionContainer>
-                        <HeaderTitle>
-                            Our links
-                        </HeaderTitle>
-                        <LinksList>
-                            <ListItem>
-                                <a href="#">
-                                    Home
-                                </a>
-                            </ListItem>
-                            <ListItem>
-                                <a href="#">
-                                    About us
-                                </a>
-                            </ListItem>
-                            <ListItem>
-                                <a href="#">
-                                    Services
-                                </a>
-                            </ListItem>
-                            <ListItem>
-                                <a href="#">
-                                    Models
-                                </a>
-                            </ListItem>
-                            <ListItem>
-                                <a href="#">
-                                    Blog
-                                </a>
-                            </ListItem>
-                        </LinksList>
-                    </SectionContainer>
-                </LinksList>
+                <SectionContainer>
+                    <HeaderTitle>
+                        Our links
+                    </HeaderTitle>
+                    <LinksList>
+                        <ListItem>
+                            <a href="#">
+                                Home
+                            </a>
+                        </ListItem>
+                        <ListItem>
+                            <a href="#">
+                                About us
+                            </a>
+                        </ListItem>
+                        <ListItem>
+                            <a href="#">
+                                Services
+                            </a>
+                        </ListItem>
+                        <ListItem>
+                            <a href="#">
+                                Models
+                            </a>
+                        </ListItem>
+                        <ListItem>
+                            <a href="#">
+                                Blog
+                            </a>
+                        </ListItem>
+                    </LinksList>
+                </SectionContainer>
+                <SectionContainer>
+                    <HeaderTitle>
+                        Other links
+                    </HeaderTitle>
+                    <LinksList>
+                        <ListItem>
+                            <a href="#">
+                                FAQ
+                            </a>
+                        </ListItem>
+                        <ListItem>
+                            <a href="#">
+                                Contact us
+                            </a>
+                        </ListItem>
+                        <ListItem>
+                            <a href="#">
+                                Support
+                            </a>
+                        </ListItem>
+                        <ListItem>
+                            <a href="#">
+                                Privacy policy
+                            </a>
+                        </ListItem>
+                        <ListItem>
+                            <a href="#">
+                                Terms &amp; Conditions
+                            </a>
+                        </ListItem>
+                    </LinksList>
+                </SectionContainer>
+                <SectionContainer>
+                    <HeaderTitle>
+                        Call now
+                    </HeaderTitle>
+                    <HorizontalContainer>
+                        <RedIcon>
+                            <FontAwesomeIcon icon={ faPhoneAlt } />
+                        </RedIcon>
+                        <SmallText>+91 555-234-8969</SmallText>
+                    </HorizontalContainer>
+                </SectionContainer>
+                <SectionContainer>
+                    <HeaderTitle>
+                        Mail
+                    </HeaderTitle>
+                    <HorizontalContainer>
+                        <RedIcon>
+                            <FontAwesomeIcon icon={ faEnvelope } />
+                        </RedIcon>
+                        <SmallText>info@yourcar.com</SmallText>
+                    </HorizontalContainer>
+                </SectionContainer>
             </InnerContainer>
         </FooterContainer>
     );
